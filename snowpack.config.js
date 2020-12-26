8551f8bd8300812879c0cd6245de4803c5fded8f
Seia-Soto <seia@outlook.kr>
2020-12-27 05:51:37 +0900

    Change to relative path

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,7 +1,7 @@
 /** @type {import("snowpack").SnowpackUserConfig } */
 module.exports = {
   mount: {
-    public: { url: '/', static: true },
+    public: { url: './', static: true },
     src: { url: '/dist' }
   },
   plugins: ['@snowpack/plugin-react-refresh'],
