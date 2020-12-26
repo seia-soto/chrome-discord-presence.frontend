41d66d86dfb94d3f870fab84c4fd19feacc56083
Seia-Soto <seia@outlook.kr>
2020-12-27 04:15:08 +0900

    Remove useless files and install chakra ui

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+import { ChakraProvider as UIProvider } from '@chakra-ui/react'
+
+const App = props => {
+  return (
+    <UIProvider />
+  )
+}
+
+export default App
